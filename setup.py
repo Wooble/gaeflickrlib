f9158ced22b58a94a89a33bc604b547aa9031f4d
Geoffrey Spear <geoffspear@gmail.com>
2014-05-06 14:23:12 -0400

    bump version, starting on test suite
    --HG--
    branch : dynamic

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 setup(name='gaeflickrlib',
-      version='0.5',
+      version='0.6',
       packages=['gaeflickrlib'],
       author='Geoffrey Spear',
       author_email='geoffspear@gmail.com',
       url='http://www.geoffreyspear.com/gaeflickrlib/index.html',
       classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Environment :: Web Environment',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: BSD License',
                    'Programming Language :: Python :: 2.5',
+                   'Programming Language :: Python :: 2.7',
                    'Topic :: Software Development :: Libraries :: Python Modules'],
       description='Flickr API kit for Google App Engine',
-      keywords=['flickr', 'google-app-engine'],
+      keywords=['flickr', 'google-app-engine', 'gae', 'appengine'],
+      install_requires=['oauth2'],
+      test_suite='gaeflickrlib.test',
       )
